034ff50166a16ab5f0b6aa2c85443c1faf9e8b54
eliasgdeveloper <eliasgdeveloper@gmail.com>
2022-12-09 20:12:46 -0300

    Subindo a Calculadora em React

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #141212;
+    background-color: #b8b4b4;
+    color: #827f7f;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Header/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  width: 50%;
+  height: 75px;
+  background-color: #054d08;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 40px;
+
+  font-size: 32px;
+  font-family: 'Arial';
+  color: #fffffd;
+`;
